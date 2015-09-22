f9c7fb0e86d33cb9109dc1865b6591364cb3c65a
tshiwakoti <tshiwakoti@outlook.com>
2015-09-21 19:36:36 -0700

    Procfile

--- server/controllers/lectures.js
+++ server/controllers/lectures.js
@@ -5,23 +5,23 @@ var lectures = mongoose.model('lecture');
 
 
 module.exports = (function() {
-return {
+  return {
 
-  add: function(req, res) {
-    console.log(req.body);
-  var new_lecture = new lectures ({ author: req.body.username, title: req.body.title, participants : req.body.participants, lecture: req.body.body});
-  console.log('testing in server controller', req.body);
+    add: function(req, res) {
+      console.log(req.body);
+      var new_lecture = new lectures ({ author: req.body.username, title: req.body.title, participants : req.body.participants, lecture: req.body.body});
+      console.log('testing in server controller', req.body);
 
-  //check if user exits
-  new_lecture.save(function(err, results){
-      if (err){
-        console.log(err);
-      }
-      else
+      //check if user exits
+      new_lecture.save(function(err, results){
+        if (err){
+          console.log(err);
+        }
+        else
         {
           res.json(results);
         }
-       console.log(results);
+        console.log(results);
       });
     },
 
